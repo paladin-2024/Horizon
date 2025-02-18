c8e6eca5a58453d7949b3986dcb18eac8becfc48
paladin-2024 <161815974+paladin-2024@users.noreply.github.com>
2025-02-18 12:11:23 +0300

    Initiating the bank card part which is an aside only on the home page with the user profile

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Sidebar from "@/components/Sidebar";
 export default function RootLayout({
 children,
@@ -9,6 +10,18 @@ children: React.ReactNode;
 return (
     <main className="flex h-screen w-full font-inter ">
         <Sidebar user={loggedIn}/>
+        
+        <div className="flex size-full flex-col">
+            <div className="root-layout">
+                <Image src="/icons/logo.svg"
+                width={30}
+                height={30}
+                alt="menu icon" />
+                    <div>
+                        
+                    </div>
+            </div>
+        </div>
         {children}
     </main>
 );
