48ff227686d92538070c04d7ca7846d8223e373c
paladin-2024 <161815974+paladin-2024@users.noreply.github.com>
2025-02-19 19:28:12 +0300

    Refactor AuthForm to support dynamic validation schema and add additional fields for sign-up

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -18,15 +18,17 @@ const AuthForm = ({ type }:{ type: string}) => {
     const [user, setUser] = useState(null)
     const [isLoading, setIsLoading] = useState(false)
 
-    const form = useForm<z.infer<typeof authFormSchema>>({
-        resolver: zodResolver(authFormSchema),
+    const formSchema = authFormSchema(type);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
         defaultValues: {
             email: "",
             password: "",
         },
     })
 
-    function onSubmit(values: z.infer<typeof authFormSchema>) {
+    function onSubmit(values: z.infer<typeof formSchema>) {
         
 
         setIsLoading(true)
@@ -73,6 +75,65 @@ const AuthForm = ({ type }:{ type: string}) => {
             <>
                 <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                        {type === 'sign-up' && (
+                            <>
+                                <div className="flex gap-4">
+                                <CustomInput
+                                    control={form.control}
+                                    name='firstName'
+                                    label='First Name'
+                                    placeholder='Enter your First Name'
+                                />
+
+                                <CustomInput
+                                    control={form.control}
+                                    name='lastName'
+                                    label='Last Name'
+                                    placeholder='Enter your Last Name'
+                                />
+                                </div>
+
+                                <CustomInput
+                                    control={form.control}
+                                    name='address1'
+                                    label='Address'
+                                    placeholder='Enter your Specific Address'
+                                />
+                                
+                                <div className="flex gap-4">
+                                    <CustomInput
+                                        control={form.control}
+                                        name='state'
+                                        label='State'
+                                        placeholder='Example: Kampala'
+                                    />
+
+                                    <CustomInput
+                                        control={form.control}
+                                        name='postalCode'
+                                        label='Postal Code'
+                                        placeholder='Example: 11101'
+                                    />
+                                </div>
+
+                                <div className="flex gap-4">
+                                    <CustomInput
+                                        control={form.control}
+                                        name='dateOfBirth'
+                                        label='Date of Birth'
+                                        placeholder='YYYY-MM-DD'
+                                    />
+
+                                    <CustomInput
+                                        control={form.control}
+                                        name='ssn'
+                                        label='SSN'
+                                        placeholder='Example: 1234'
+                                    />
+                                </div>
+                                
+                            </>
+                        )}
 
                         <CustomInput
                         control={form.control}
